b53cf270474752708da784acf74f68a18eae8625
SpaceKun <drwork97@gmail.com>
2020-05-30 15:03:29 +0300

    Переписал игру на 32стр

--- game.rb
+++ game.rb
@@ -4,9 +4,12 @@ puts "Привет, как тебя зовут?"
 nam = gets.chomp.to_s
 number = rand(1..15)
 puts "У тебя есть 3 попытки что бы угадать число"
-a = gets.chomp.to_i
+
+n = 0
 
 # first
+while n <= 2 do
+	a = gets.chomp.to_i
 if a == number
 	puts 'Вы угадали, поздравляю Вас ' + nam
 	exit
@@ -24,46 +27,49 @@ if a == number
 		end
 	end
 end
-
-# second
-a = gets.chomp.to_i
-if a == number
-	puts 'Вы угадали, поздравляю Вас ' + nam
-	exit
-	else
-
-	if a > number
-		b = "нужно меньше"
-	else
-		b = "нужно больше"
-		
-		if (a - number).abs <3
-			puts "Тепло, " + b.to_s 
-		else
-			puts "Холодно, " + b.to_s 
-		end
-	end
+n += 1
 end
-
-
-# third
-a = gets.chomp.to_i
-if a == number
-	puts 'Вы угадали, поздравляю Вас ' + nam
-	exit
-	else
-
-	if a > number
-		b = "нужно меньше"
-	else
-		b = "нужно больше"
-		
-		if (a - number).abs <3
-			puts "Тепло, " + b.to_s 
-		else
-			puts "Холодно, " + b.to_s 
-		end
-	end
-end
-
 puts 'В этот раз вам не повезло. Было загадано число ' + number.to_s
+
+# # second
+# a = gets.chomp.to_i
+# if a == number
+# 	puts 'Вы угадали, поздравляю Вас ' + nam
+# 	exit
+# 	else
+
+# 	if a > number
+# 		b = "нужно меньше"
+# 	else
+# 		b = "нужно больше"
+		
+# 		if (a - number).abs <3
+# 			puts "Тепло, " + b.to_s 
+# 		else
+# 			puts "Холодно, " + b.to_s 
+# 		end
+# 	end
+# end
+
+
+# # third
+# a = gets.chomp.to_i
+# if a == number
+# 	puts 'Вы угадали, поздравляю Вас ' + nam
+# 	exit
+# 	else
+
+# 	if a > number
+# 		b = "нужно меньше"
+# 	else
+# 		b = "нужно больше"
+		
+# 		if (a - number).abs <3
+# 			puts "Тепло, " + b.to_s 
+# 		else
+# 			puts "Холодно, " + b.to_s 
+# 		end
+# 	end
+# end
+
+# puts 'В этот раз вам не повезло. Было загадано число ' + number.to_s
